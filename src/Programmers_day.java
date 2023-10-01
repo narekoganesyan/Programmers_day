3a1cd3b967ee7f10d8fbde556689983b5b9f3d00
narekoganesyan <narek.oganesyan.03@mail.ru>
2023-10-02 00:40:46 +0300

    123

--- src/Programmers_day.java
+++ src/Programmers_day.java
@@ -1,3 +1,31 @@
+import java.util.Scanner;
+
 public class Programmers_day {
+
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите год: ");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println("13.09." + year);
+        } else {
+            System.out.println("12.09." + year);
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 
 }
